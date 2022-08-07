e9872602c32b7b419972df3ecae8294843a7ccdd
Seungjun Kim <imscott1009@gmail.com>
2022-08-07 12:31:36 +0900

    file name change

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pjHire.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hire.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
