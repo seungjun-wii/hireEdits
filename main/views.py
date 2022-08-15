7236429ec99058a451b4e9fa61d726bfd216b81a
Seungjun Kim <imscott1009@gmail.com>
2022-08-16 08:26:26 +0900

    template set

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse;
 # Create your views here.
 
 def home(response):
-    return render(response, "main/index.html", {})
+    return render(response, "main/homepage.html", {})
