2e44bcb246404c3c5fb7f136a8a46431ecb8e421
Seungjun Kim <imscott1009@gmail.com>
2022-08-23 13:52:48 +0900

    opportunity page

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("about", views.about, name="about"),
+    path("opportunity", views.opport, name="opport")
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse;
 
 def home(response):
     return render(response, "main/homepage.html", {})
+
+def about(response):
+    return render(response, "main/about.html", {})
+
+def opport(response):
+    return render(response, "main/opport.html", {})
