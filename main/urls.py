259def86c902d8ec4e681db433543e9964840bc5
Seungjun Kim <imscott1009@gmail.com>
2022-09-03 18:14:11 +0900

    major update

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about", views.about, name="about"),
-    path("opportunity", views.opport, name="opport")
+    path("opportunity", views.opport, name="opport"),
+    path("profile", views.profile, name="profile"),
+    path("profile/edit", views.edit, name="profile"),
+    path("opportunity/company1", views.company1, name="company"),
+    path("login", views.login, name="login")
 ]
--- main/views.py
+++ main/views.py
@@ -11,3 +11,15 @@ def about(response):
 
 def opport(response):
     return render(response, "main/opport.html", {})
+
+def profile(response):
+    return render(response, "main/profile.html", {})
+
+def edit(response):
+    return render(response, "main/edit.html", {})
+
+def company1(response):
+    return render(response, "main/comp.html", {})
+
+def login(response):
+    return render(response, "main/login.html", {})
